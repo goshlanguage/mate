3fbc25a87a00aa9d9f87bdab7d955912f90d80e3
Ryan Hartje <hartjepc@gmail.com>
2021-11-15 22:32:52 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,64 @@
+mate
+===
+
+## Mini Algorithmic Trading Engine
+
+`mate` is an experimental trading engine project. Use at your own risk.
+
+Mate supports the following brokerages:
+
+|Broker|Description|
+|-|-|
+|TD Ameritrade|See [TD Ameritrade developer portal](https://developer.tdameritrade.com/user) and [TDA SDK Crate source](https://github.com/rideron89/tda-sdk-rs/) in use for this project|
+
+
+## Getting Started
+
+You will need access to an API with live market data. Currently only supporting [`TD Ameritrade`](#TD-Ameritrade-Setup).
+
+### Configuration
+
+Configuration is passed in through environment only currently:
+
+|Env Var|Description|
+|-|-|
+|`TDA_CLIENT_ID`|OAuth Client ID from your TD Ameritrade developer account, appending your oAuth domain eg: `@AMER.OAUTHAP`|
+|`TDA_REFRESH_TOKEN`|Oauth refresh token to renew oauth access token, see [`TD Ameritrade`](#TD-Ameritrade-Setup) for |
+
+
+### TD Ameritrade Setup
+
+To register an account and get API access, you can either see [upstream documentation](https://developer.tdameritrade.com/content/getting-started#createAccount), or follow the steps below:
+
+- [Sign up](https://developer.tdameritrade.com/user/register) for a developer account, even if you have a brokerage account
+- [Create your application](https://developer.tdameritrade.com/user/me/apps/add) (if you plan to run this locally, use `http://localhost` or `http://127.0.0.1` for the `redirect_url`)
+- go to `https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=<Your url encoded callback url>&client_id=<Your Client Key>%40AMER.OAUTHAP` in a browser, replacing `<Your url encoded callback url>` with the `redirect_url` you created when you created your TD Ameritrade app, and replacing `<Your Client Key>` with the client key from your TD Ameritrade app.
+  - eg `https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost&client_id=MYKEY123%40AMER.OAUTHAP`
+- This should redirect you to a log in for your TD ameritrade account (brokerage account), to perform the oauth grant to give your app access to your account
+- if you used localhost, it will redirect and be broken, but the code to fetch your access and refresh token will be in the redirected url query params (eg: `https://localhost/?code=Your%2Code%2Here`)
+- take that code, and go to [https://developer.tdameritrade.com/authentication/apis/post/token-0](https://developer.tdameritrade.com/authentication/apis/post/token-0)
+  - enter `authorization_code` for `grant_type`
+  - enter `offline` for `access_type`
+  - urldecode the code you copied from the redirect earlier
+    ```sh
+    ipython -c "import urllib; urllib.parse.unquote(\"YOURCODEHERE\")"
+    ```
+  - enter the urldecoded string into `code`
+  - enter your application's given `client_id` into `client_id` (should end in `@AMER.OAUTHAP`)
+  - enter your application's given `redirect_uri`
+  - click `send`
+  - the JSON populated at the bottom of the page contains an `access_token` and a `refresh_token`, be sure to save the `refresh_token`, and set it in your environment (export it in your shell rc eg: `~/.bashrc` or `~/.zshrc`)
+
+> note: if you end up with a failing `grant_invalid` error, you might want to make sure you're unquoting your authorization code, otherwise see:
+>
+> https://developer.tdameritrade.com/content/authentication-faq
+
+## TODOs
+
+- Get away from TODOs
+- open the user's browser to grab the refresh token when they start the application
+- create a listener on local host to intercept the refresh token -> use the query params to fetch the authorization code, then exit listener and request the refresh token with authorization code.
+- Setup paper trading
+- Setup a collector for future backtesting feature
+- Setup API to feed into a web dashboard
+
---
+++ src/main.rs
@@ -0,0 +1,144 @@
+use chrono::Utc;
+use chrono::prelude::*;
+use std::{
+    collections::HashMap,
+    env,
+    thread,
+    time::Duration,
+};
+use tda_sdk::{
+    AccessToken,
+    Client,
+    params::GetAccountsParams,
+    params::GetPriceHistoryParams,
+    responses::SecuritiesAccount,
+};
+
+// mate makes use of the tda-sdk crate for access to a brokerage API
+// https://github.com/rideron89/tda-sdk-rs
+
+fn main() {
+    let mate = Mate::new();
+    println!("{}", mate.status());
+
+    loop {
+        let (ema20, err) = mate.ema("MSFT".to_string(), 20);
+        if err {
+            println!("error computing ema20");
+        }
+
+        let (ema50, err) = mate.ema("MSFT".to_string(), 50);
+        if err {
+            println!("error computing ema50");
+        }
+
+        if ema20 > ema50 {
+            println!("buy");
+        } else {
+            println!("sell");
+        }
+
+        thread::sleep(Duration::from_secs(60));
+    }
+}
+
+
+pub struct Mate {
+    client: Client,
+    ema_cache: HashMap<String, String>,
+}
+
+impl Mate {
+    pub fn new() -> Self {
+        let (client_id, refresh_token) = get_creds();
+        let mut client = Client::new(&client_id, &refresh_token, None);
+
+        let access_token: AccessToken = client.get_access_token().unwrap().into();
+
+        // TODO
+        // this function seems to not work currently
+        // This needs a working expiry check, so when expired, a token can be refreshed
+        if access_token.has_expired() {
+            let _dt = Utc.timestamp(access_token.expires_at, 0);
+            let _now = Utc::now();
+            // println!("Current access token is supposedly expired!\nCurrent time: {}", now.to_rfc2822());
+            // println!("Access token expiry: {}", dt.to_rfc2822())
+        }
+
+        client.set_access_token(&Some(access_token.into()));
+
+        return Mate{
+            client: client,
+            ema_cache: HashMap::new(),
+        }
+    }
+
+    // status reflects a TD Ameritrade account balance and other information.
+    pub fn status(&self) -> String {
+        let mut status = String::new();
+
+        let accounts = self.client.get_accounts(GetAccountsParams::default()).unwrap();
+
+        for account in accounts {
+            match account.securities_account {
+                SecuritiesAccount::MarginAccount { is_day_trader, projected_balances, .. } => {
+                    // status = format!("{}Account ID: {}\n", status, account_id);
+                    // status = format!("{}Account Type: {}\n", status, r#type);
+                    status = format!("{}Account Balance: ${}\n", status, projected_balances.cash_available_for_withdrawal);
+                    status = format!("{}Account DayTrader: {}\n", status, is_day_trader);
+                }
+            }
+        }
+        return status.to_string()
+    }
+
+    // https://www.investopedia.com/terms/e/ema.asp
+    // symbol - ticker symbol of the security you want to query
+    // period - number in days to grab info for
+    //
+    // ema first checks the ema cache, and if unpopulated, computes a table of ema for as many candles as we get back
+    // then returns the last element of the ema cache, which should be the most recent.
+    //
+    // returns computed ema as f64, and a bool if an error was encountered
+    // TODO better error handling
+    fn ema(&self, symbol: String, period: i8) -> (f64, bool) {
+        if self.ema_cache.is_empty() {
+            // https://developer.tdameritrade.com/price-history/apis/get/marketdata/%7Bsymbol%7D/pricehistory
+            let params = GetPriceHistoryParams::default();
+            let history = self.client.get_price_history(&symbol, params);
+
+            let resp = match history {
+                Ok(val) => val,
+                Err(e) => {
+                    println!("Failed to get price history: {}", e.to_string());
+                    return (0.0, true)
+                },
+            };
+            println!("Price history for {}: {:?}", resp.symbol, resp);
+
+            for i in 0..resp.candles.len() {
+                let candle = resp.candles.get(i).unwrap();
+
+                println!("Close: {}", candle.close);
+            }
+        }
+
+
+        return (0.0, true);
+    }
+}
+
+// get_creds looks for `TDA_CLIENT_ID` and `TDA_REFRESH_TOKEN` environment variables and panics if they aren't present.
+fn get_creds() -> (String, String) {
+    let client_id = match env::var("TDA_CLIENT_ID") {
+        Ok(val) => val,
+        Err(e) => panic!("Didn't find the TDA_CLIENT_ID env var, please set this and try again. {}", e)
+    };
+
+    let refresh_token = match env::var("TDA_REFRESH_TOKEN") {
+        Ok(val) => val,
+        Err(e) => panic!("Didn't find the TDA_REFRESH_TOKEN env var, please set this and try again. {}", e),
+    };
+
+    return (client_id, refresh_token)
+}
